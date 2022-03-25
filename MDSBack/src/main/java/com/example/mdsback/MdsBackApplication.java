abab872788be9575b0f6a7c093c61761a0150c2b
Razoku <razvan.gabriel.budaca@gmail.com>
2022-03-25 21:51:44 +0200

    Initialized spring boot project + react with working magentajs

---
+++ MDSBack/src/main/java/com/example/mdsback/MdsBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdsBackApplication.class, args);
+    }
+
+}
