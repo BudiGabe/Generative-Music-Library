c9ef85a7129b4983dbb9535de7ca824ef9427c30
Marius <marius.dumitrescu@s.unibuc.ro>
2022-04-06 01:14:11 +0300

    Am creat pachetul RegisterLogin care contine clasele Register si Login

--- MDSBack/src/main/java/com/example/mdsback/MdsBackApplication.java
+++ MDSBack/src/main/java/com/example/mdsback/MdsBackApplication.java
@@ -1,13 +1,33 @@
 package com.example.mdsback;
 
+import com.example.mdsback.entities.Login;
+import com.example.mdsback.entities.Register;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import Entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 @SpringBootApplication
+@RestController
 public class MdsBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(MdsBackApplication.class, args);
-        User user = new User();
+    }
+    @GetMapping
+    public List<Register> show()
+    {
+        return List.of(new Register(
+                3,
+                "Mihailescu",
+                "George",
+                "george@hotmail.com",
+                "075567289077",
+                "password123", LocalDate.of(2001, Month.APRIL,25)));
+
     }
 
 }
