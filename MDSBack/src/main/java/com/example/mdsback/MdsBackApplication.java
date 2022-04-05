3c9a524985d925d2299beda8fede4f65b83245ce
Marius <marius.dumitrescu@s.unibuc.ro>
2022-04-06 00:16:30 +0300

    SDK changed

--- MDSBack/src/main/java/com/example/mdsback/MdsBackApplication.java
+++ MDSBack/src/main/java/com/example/mdsback/MdsBackApplication.java
@@ -2,12 +2,12 @@ package com.example.mdsback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import Entities.User;
 @SpringBootApplication
 public class MdsBackApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MdsBackApplication.class, args);
+        User user = new User();
     }
 
 }
