1ee9b5a2245b7df6d24b9f313c2ff7afbe7fa168
Razoku <razvan.gabriel.budaca@gmail.com>
2022-05-03 22:02:22 +0300

    Implemented endpoint to like samples

--- MDSBack/src/main/java/com/example/mdsback/controllers/SampleController.java
+++ MDSBack/src/main/java/com/example/mdsback/controllers/SampleController.java
@@ -3,10 +3,10 @@ package com.example.mdsback.controllers;
 import com.example.mdsback.DTOs.SampleDTO;
 import com.example.mdsback.models.Sample;
 import com.example.mdsback.repositories.SampleRepository;
+import com.example.mdsback.services.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,10 +17,12 @@ public class SampleController {
     @Autowired
     private SampleRepository sampleRepo;
 
+    @Autowired
+    private SampleService sampleService;
+
     @GetMapping("/{id}")
     public SampleDTO findById(@PathVariable Long id) {
-        Sample sample = sampleRepo.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Sample not found"));
+        Sample sample = sampleService.findById(id);
         return new SampleDTO(sample);
     }
 
@@ -36,5 +38,12 @@ public class SampleController {
     @ResponseStatus(HttpStatus.CREATED)
     public Sample create(@RequestBody Sample sample) {
         return sampleRepo.save(sample);
+    }
+
+    @PutMapping("/like/{id}")
+    public SampleDTO likeSample(@PathVariable Long id) {
+        Sample sample = sampleService.findById(id);
+        sampleService.incrementLikes(sample);
+        return new SampleDTO(sampleRepo.save(sample));
     }
 }
