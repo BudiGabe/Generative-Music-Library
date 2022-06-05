553d9f117b572eeb677775877b622fcd185380da
Razoku <razvan.gabriel.budaca@gmail.com>
2022-06-05 23:00:13 +0300

    Added Top Samples page

--- react-test/src/components/TopSamples.jsx
+++ react-test/src/components/TopSamples.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { getSamplesTop } from "../Services/sample-service";
+import Sample from "./Sample";
 
-function TopSamples() {
-  return (
-    <div>
-        TopSamples
-    </div>
-  )
+class TopSamples extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            samples: []
+        }
+    }
+
+    componentDidMount = () => {
+        getSamplesTop().then(samples => this.setState({samples: samples}))
+    }
+
+    render() {
+        const { samples } = this.state
+        return (
+            <div>
+                {samples.map(sample => <div key={sample.id}><Sample  sample={sample}/></div>)}
+            </div>
+        )
+    }
 }
 
 export default TopSamples
