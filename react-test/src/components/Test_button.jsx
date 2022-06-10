b02e71c9938a944aa2e8e42fdda6ca3682e91a88
DanTheAssassin <dantheassassin1702@gmail.com>
2022-06-10 21:01:01 +0300

    incercare upload_sample incomplet

--- react-test/src/components/Test_button.jsx
+++ react-test/src/components/Test_button.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import {upload_sample} from "../Services/magenta-service";
 import {play_sample} from "../Services/magenta-service";
-import {Button} from "@mui/material";
 
 function Test_button(){
 
     //return upload_sample("testare.mid");
     return(
-        <Button onClick={() => {
-         //   console.log(upload_sample("testare.mid"));
+        <button type="button" value="testare" onClick={() => {
+           console.log(upload_sample("testare.mid"));
         }}>
-
-        </Button>
+            Testare
+        </button>
     )
 }
 
