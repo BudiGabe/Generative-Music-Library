ffee4c1d726c80fd03495f64dca6e6d94b2feaf1
Razoku <razvan.gabriel.budaca@gmail.com>
2022-06-12 17:12:44 +0300

    Added completely functional Register page

--- react-test/src/components/Register.jsx
+++ react-test/src/components/Register.jsx
@@ -1,22 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as authService from "../Services/auth-service";
 
 function Register() {
-  return (
-    <div className='registerpage'>
-        <div className="container">
-            <h2>Create your account</h2>
-            <form>
-                <input placeholder='Username'></input>
-                <input type="email" placeholder='E-mail'></input>
-                <input type="password" placeholder='Password' />
-                <div className="termsandcontions">
-                <input type="checkbox" className='checkboxbtn'></input><h3>I agree to the terms and conditions</h3></div>
-                <button className='registerbtn'><h3>Register</h3></button>
-            </form>
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [producer, setProducer] = useState('')
+
+    const handleUsername = (e) => {
+        setUsername(e.target.value);
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    }
+    const handleProducer = (e) => {
+        setProducer(e.target.checked);
+    }
+
+    function handleSubmit(e) {
+        let user = {
+            name: username,
+            password: password,
+            roles: [
+                {name: "USER"}
+            ]
+        }
+
+        if (producer === true) {
+            user.roles.push({name: "PRODUCER"})
+        }
+
+        e.preventDefault()
+        authService.register(user).then(response => console.log(response))
+
+    }
+
+    return (
+        <div className='registerpage'>
+            <div className="container">
+                <h2>Create your account</h2>
+                <form>
+                    <input value={username} onChange={handleUsername} id={"username"}
+                           placeholder='Username'/>
+                    <input value={password} onChange={handlePassword} id={"password"} type="password"
+                           placeholder='Password'/>
+                    <div className="producer">
+                        <input value={producer} onChange={handleProducer} id={"producer"} type="checkbox"
+                               className='checkboxbtn'/><h3>I am a producer</h3></div>
+                    <button onClick={(e) => handleSubmit(e)} type="submit" className='registerbtn'><h3>Register</h3>
+                    </button>
+                </form>
+            </div>
+
         </div>
-        
-    </div>
-  )
+    )
 }
 
 export default Register
